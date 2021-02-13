b2f4329dadd0fc6c2ccdbd09b7dd2a19a49d0358
Pendergast <coolhandkuba@hotmail.com>
2021-02-13 09:08:44 -0500

    added routes and server.js

---
+++ routes/html-routes.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const router = require('express').Router();
+
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get('/exercise?', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+
+// Test later
+// Place this route below all others to send the index.html file
+// to any request that is not explicitly defined above
+// router.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
+
+
+
+module.exports = router;
+
